fa1046ede1ddb3834353a28b1c7240dce6f246c9
Kenyonw2007 <kenyonw2007@gmail.com>
2024-09-10 22:26:21 -0500

    Add files via upload

---
+++ mutiple lines hello world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl; 
+    cout << "die with a smile" << endl;
+    cout << "so pick me , choose me , love me" << endl;
+     cout << "my name is kenyon" << endl;
+    
+     return 0; 
+}
